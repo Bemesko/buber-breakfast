c2776f7f63cdcd62092b1c2faa8e2f3e8fb4b1d4
Bernardo Mesko <bemesko@gmail.com>
2023-03-20 20:26:05 -0300

    Create method to implicitly convert a request to a model Breakfast object

--- BuberBreakfast/Controllers/BreakfastsController.cs
+++ BuberBreakfast/Controllers/BreakfastsController.cs
@@ -18,14 +18,7 @@ public class BreakfastsController : ApiController
     [HttpPost]
     public IActionResult CreateBreakfast(CreateBreakfastRequest request)
     {
-        ErrorOr<Breakfast> requestToBreakfastResult = Breakfast.Create(
-            request.Name,
-            request.Description,
-            request.StartDateTime,
-            request.EndDateTime,
-            request.Savory,
-            request.Sweet
-        );
+        ErrorOr<Breakfast> requestToBreakfastResult = Breakfast.From(request);
 
         if (requestToBreakfastResult.IsError)
         {
@@ -57,15 +50,7 @@ public class BreakfastsController : ApiController
     [HttpPut("{id:guid}")]
     public IActionResult UpsertBreakfast(Guid id, UpsertBreakfastRequest request)
     {
-        ErrorOr<Breakfast> requestToBreakfastResult = Breakfast.Create(
-            request.Name,
-            request.Description,
-            request.StartDateTime,
-            request.EndDateTime,
-            request.Savory,
-            request.Sweet,
-            id
-        );
+        ErrorOr<Breakfast> requestToBreakfastResult = Breakfast.From(id, request);
 
         if (requestToBreakfastResult.IsError)
         {
--- BuberBreakfast/ServiceErrors/Errors.Breakfast.cs
+++ BuberBreakfast/ServiceErrors/Errors.Breakfast.cs
@@ -9,12 +9,12 @@ public static class Errors
         public static Error InvalidName => Error.Validation(
             code: "Breakfast.InvalidName",
             description: $"Breakfast name must be at least {Models.Breakfast.MIN_NAME_LENGTH}" +
-            $"characters long and at most {Models.Breakfast.MAX_NAME_LENGTH} characters long.");
+            $" characters long and at most {Models.Breakfast.MAX_NAME_LENGTH} characters long.");
 
         public static Error InvalidDescription => Error.Validation(
             code: "Breakfast.InvalidDescription",
             description: $"Breakfast description must be at least {Models.Breakfast.MIN_DESCRIPTION_LENGTH}" +
-            $"characters long and at most {Models.Breakfast.MAX_DESCRIPTION_LENGTH} characters long.");
+            $" characters long and at most {Models.Breakfast.MAX_DESCRIPTION_LENGTH} characters long.");
 
         public static Error NotFound => Error.NotFound(
             code: "Breakfast.NotFound",
